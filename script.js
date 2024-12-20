a78b21f3f033c6742da73c2930967e0381e687e7
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-12-20 09:51:10 -0600

    add script

--- script.js
+++ script.js
@@ -87,3 +87,42 @@ function updateParagraphsValue(){
                 ? `<${tag}>${words}</${tag}>`
                 : words;
     }
+   // Function to generate a specified number of words
+function generateWords(numWords) {
+    
+    // Lorem Ipsum text for demonstration purposes
+    const loremIpsumText =
+        `Lorem ipsum dolor sit amet, consectetur 
+        adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna 
+        aliqua. Diam in arcu cursus euismod 
+        quis viverra nibh. Nunc aliquet bibendum
+        enim facilisis gravida neque convallis 
+        a cras. Sagittis purus sit amet volutpat
+        Consequat mauris. Duis ultricies lacus 
+        sed turpis tincidunt id. Consequat interdum
+        varius sit amet mattis vulputate. Enim sed
+        faucibus turpis in eu. Ridiculus mus mauris
+        vitae ultricies leo integer malesuada nunc vel.
+        Nulla pharetra diam sit amet nisl suscipit.
+        Lobortis elementum nibh tellus molestie nunc
+        non blandit massa enim. Dis parturient montes
+        nascetur ridiculus mus. Justo nec ultrices dui
+        sapien eget. Enim tortor at auctor urna nunc.
+        Dictumst quisque sagittis purus sit amet volutpat
+        consequat mauris nunc.`;
+
+
+    // Split the Lorem Ipsum text into words
+    const words =
+        loremIpsumText.split(" ");
+         // Ensure the number of words requested is 
+    // within the bounds of the available words
+    if (numWords <= words.length) {
+        return words
+            .slice(0, numWords)
+            .join(" ");
+    } else {
+        return words.join(" ");
+    }
+}
